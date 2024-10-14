af292e8229c6027a8af461e3aa66759c89812b97
joshafouda <josh.aremou229@gmail.com>
2024-10-14 16:22:51 +0200

    build and puplication OK

---
+++ example_script.py
@@ -0,0 +1,13 @@
+import impyrialtuto
+
+#from impyrialtuto.length import convert_unit
+
+#result_length = convert_unit(500, from_unit="yd", to_unit="ft")
+
+#print(result_length)
+
+from impyrialtuto.weight import convert_unit
+
+result_weight = convert_unit(100, from_unit="oz", to_unit="lb")
+
+print(result_weight)
